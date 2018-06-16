ab0f1038781c9db7f356526400741da8e99bc3c9
Dave Dews <dave.dews@gmail.com>
2018-06-16 08:19:28 +0100

    Added actions and reducers for retrieving data from firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -51,3 +51,48 @@ export const editExpense = (id, updates) => ({
   updates
 });
 
+// SET_EXPENSES
+export const setExpenses = (expenses) => ({
+  type: 'SET_EXPENSES',
+  expenses
+});
+
+
+export const startSetExpenses = () => {
+  return (dispatch) => {
+    return database.ref('expenses')
+    .once('value')
+    .then((snapshot) => {
+        const expenses = [];
+
+        snapshot.forEach((childSnapshot) => {
+          expenses.push({
+            id: childSnapshot.key,
+            ...childSnapshot.val()
+          }); 
+        });
+
+        dispatch(setExpenses(expenses));
+      });
+  };
+  // const expenses = [];
+  // database.ref('expenses')
+  // .once('value')
+  // .then((snapshot) => {
+  //   snapshot.forEach((childSnapshot) => {
+  //     expenses.push({
+  //       id: childSnapshot.key,
+  //       ...childSnapshot.val()
+  //     });
+  //   });
+  //   console.log(expenses);
+  // }).then(() => {
+  //   return dispatch(setExpenses({
+  //           ...expenses
+  //         }))
+  // }).catch((e) => {
+  //   console.log('Error thrown reading expenses from Firebase db.', e);
+  // });
+
+};
+
--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import AppRouter from './routers/AppRouter';
-import { addExpense, removeExpense, editExpense } from './actions/expenses';
-import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from './actions/filters';
 import configureStore from './store/configureStore';
+import { startSetExpenses } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -20,4 +20,10 @@ const jsx = (
     <AppRouter />
   </Provider>
 );
-ReactDOM.render(jsx, document.getElementById('app'));
+
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+  ReactDOM.render(jsx, document.getElementById('app'));
+});
+
